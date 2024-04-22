193019cd33d27ce34e543d7236c68c755d6c878d
Sathya112716 <sathyapriya112716@gmail.com>
2024-04-22 23:26:43 +0530

    read the delta table and perform the required things

--- src/assignment_1/silver_to_gold/employee_silver_to_gold.py
+++ src/assignment_1/silver_to_gold/employee_silver_to_gold.py
@@ -11,10 +11,6 @@ employee_df = spark.read.format("delta").load("dbfs:/user/hive/warehouse/employe
 
 # COMMAND ----------
 
-display(employee_df)
-
-# COMMAND ----------
-
 from pyspark.sql.functions import avg,desc,count
 
 # COMMAND ----------
@@ -29,27 +25,12 @@ salary_by_department = employee_df.groupBy("department").agg(avg("salary").alias
 department_join_df = employee_df.join(department_df, employee_df.department == department_df.department_id, "inner")
 country_join_df = department_df.join(country_df, department_df.country_name == country_with_date_df.country_code, "inner")
 department_with_country = country_df.select('department_name', 'country_name')
-display(department_with_country)
-
-# COMMAND ----------
 
 employee_count = employee_df.groupBy("department", "country").agg(count("employee_id").alias("employee_count"))
-display(employee_count)
-
-
-# COMMAND ----------
 
 # What is the average age of employees in each department?
 avg_age_by_department = employee_df.groupBy('department').agg(avg("age").alias('avg_age'))
-display(avg_age_by_department)
-
-
-# COMMAND ----------
 
 #Add the at_load_date column to data frames. 
-
-
-# COMMAND ----------
-
 
 employee_df.write.format("parquet").mode("overwrite").option("replaceWhere","load_date = '2024-04-16'").save("/FileStore/src/assignment_1/gold/employee/table_name")
